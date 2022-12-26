316f4088951a9a1d491a42da4234e1421e76fa1a
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2022-12-26 14:33:29 +0530

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -5,7 +5,6 @@ class Login extends MY_Controller{
 
 		if( $this->session->userdata('user_id') )
 			return redirect('admin/dashboard');
-	   // The above two lines checks, if the user is logged in, then it redirects to dashboard
 
 		$this->load->helper(array('url','form'));
 		$this->load->view('public/admin_login');
@@ -14,11 +13,6 @@ class Login extends MY_Controller{
 		{
 			$this->load->library('form_validation');
 			$this->load->helper('url');
-			// $this->form_validation->set_rules('username','Username','required|alpha|trim');
-			// $this->form_validation->set_rules('password','Password','required');	
-			
-            // The above two lines is commented because we have set the rules in config > form_validation.php
-
 			$this->form_validation->set_error_delimiters("<p class='text-danger'>","</p>");
 
 		if( $this->form_validation->run('admin_login') ){
@@ -27,22 +21,16 @@ class Login extends MY_Controller{
 			$password=$this->input->post('password');
 			
 			$this->load->model('loginmodel');
-		    // The above line loads the model i.e the database part, and the below line will check the credentails entered.
 			$login_id = $this->loginmodel->login_valid($username, $password);
 			if( $login_id )
 			{
 				$this->load->library('session');
 				$this->session->set_userdata( 'user_id',$login_id ); 
 				return redirect('admin/dashboard');
-				// The above line is redirecting to dashboard, because it if we write $this->load->view('admin/dashboard'), then it may ruin the logic, as the url will be like "tms/index.php/login/admin_login", hence we keep login.php to function as Backend only for login. The view will be loaded in Admin Controller. 
+			} else {
+				$this->session->set_flashdata('login_failed','Invalid Username/Password.'); 
+				return redirect('login');
 			}
-				else{
-
-					$this->session->set_flashdata('login_failed','Invalid Username/Password.'); 
-					return redirect('login');
-					//echo "Credentials did not match";	
-					//Credentails Failed
-				}
 
 		  } 	 
 			else{
