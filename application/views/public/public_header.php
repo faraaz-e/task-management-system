26283d0fe0f33bc10d944676f423f5a2893acce3
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2022-12-26 14:42:58 +0530

    Update public_header.php

--- application/views/public/public_header.php
+++ application/views/public/public_header.php
@@ -5,12 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?=base_url()?>/favicon.png" type="image/png">
 	<title>TMS</title>
-	<!-- <link rel="stylesheet" type="text/css" href="http://htdocs/ci/assets/css/bootstrap.min.css"> -->
-	<!-- The above line is commented, to make URL dynamic, in case of migrating it from localhost to Live Server.
-	Below Line is Dynamic. -->
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
    <link rel="javascript" type="text/javascript" href="<?= base_url('assets/js/bootstrap.min.js'); ?>">
-   <!-- The above line will be loaded using Helper in User.php(Controller)-->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  
    
@@ -20,12 +16,6 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
-
-
- <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> -->
- <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.0/js/bootstrap-datepicker.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.0/css/bootstrap-datepicker.css" rel="stylesheet" /> -->
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -49,22 +39,9 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarColor02">
-    <ul class="navbar-nav mr-auto">
-     <!-- <li class="nav-item active">
-        <a class="nav-link" href="<?= base_url('index.php'); ?>">Task List <span class="sr-only">(current)</span></a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="#">About</a>	
-      </li> -->
-    </ul>
-
 
     <ul class="navbar-nav mr-auto">
    <?= form_open('user/search',['class'=>'form-inline my-2 my-lg-0', 'role'=>'search']) ?> 
-   <!-- <form class="form-inline my-2 my-lg-0"> -->
       <input class="form-control mr-sm-2"  type="text" name="query" placeholder="Query">
       <button class="btn btn-secondary my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
     <?= form_close(); ?>
@@ -80,8 +57,6 @@
     </ul>
 </div>
 </nav>
-<!-- <div style="background-image: url('assets/images/bg-image1.jpg');"> -->
-  <!-- <div style="background-color: #f2f2f2"> -->
 <br/>
 
 
