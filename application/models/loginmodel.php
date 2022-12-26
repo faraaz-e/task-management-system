865ad0545c7df314f8da3b129770e62baac9b2d9
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2022-12-26 14:38:38 +0530

    Update loginmodel.php

--- application/models/loginmodel.php
+++ application/models/loginmodel.php
@@ -4,18 +4,15 @@ class Loginmodel extends CI_Model {
 
 	public function login_valid( $username, $password )
 	{
-		// $this->load->database();
-		// Database is loaded in Autoload instead of writing the above line here.
 		$q= $this->db->query(" SELECT * FROM users WHERE 
 							 uname='$username' AND 
 							 pswrd='$password' ; ");
 
 		if ( $q->num_rows() ){
 			return $q->row()->id;
-				// return TRUE;
 		}
 		else{
-				return FALSE;
+			return FALSE;
 		} 
 	}
 
