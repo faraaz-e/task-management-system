74b21b55d1fc3d167f648a8f553a53882c9e95de
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2022-12-26 14:30:09 +0530

    Update admin.php

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -4,121 +4,95 @@ class Admin extends MY_Controller{
 	public function dashboard() 
 	{
 
-			$this->load->library('pagination');
+		$this->load->library('pagination');
 
-			$config = [
-				'base_url'  => base_url('admin/dashboard'),
-				'per_page'  => 5,
-				'total_rows' => $this->articles->num_rows(),
+		$config = [
+			'base_url'  => base_url('admin/dashboard'),
+			'per_page'  => 5,
+			'total_rows' => $this->articles->num_rows(),
 
-				'full_tag_open' => "<ul class='pagination'>",
-				'full_tag_close' => '</ul>',
+			'full_tag_open' => "<ul class='pagination'>",
+			'full_tag_close' => '</ul>',
 
-				'first_tag_open' => '<li class="page-item">',
-				'first_tag_close' => '</li>',
+			'first_tag_open' => '<li class="page-item">',
+			'first_tag_close' => '</li>',
 
-				'last_tag_open' => '<li class="page-item">',
-				'last_tag_close' => '</li>',
+			'last_tag_open' => '<li class="page-item">',
+			'last_tag_close' => '</li>',
 
-				'next_tag_open' => '<li class="page-item">',
-				'next_tag_close' => '</li>',
+			'next_tag_open' => '<li class="page-item">',
+			'next_tag_close' => '</li>',
 
-				'prev_tag_open' => '<li class="page-item">',
-				'prev_tag_close' => '</li>',
+			'prev_tag_open' => '<li class="page-item">',
+			'prev_tag_close' => '</li>',
 
-				'num_tag_open' => '<li class="page-item">',
-				'num_tag_close' => '</li>',
+			'num_tag_open' => '<li class="page-item">',
+			'num_tag_close' => '</li>',
 
-				'cur_tag_open' => '<li class="page-item active bg-dark text-white"><a>',
-				'cur_tag_close' => '</a></li>',
-			];
+			'cur_tag_open' => '<li class="page-item active bg-dark text-white"><a>',
+			'cur_tag_close' => '</a></li>',
+		];
 
-			$this->pagination->initialize($config);
-
-
+		$this->pagination->initialize($config);
 		$this->load->helper('url');
-		// $this->load->model('articlesmodel','articles');
-		// The above line is commented because it is now written in common in constructor.
 		$articles = $this->articles->articles_list( $config['per_page'], $this->uri->segment(3) );
-
 		$this->load->view('admin/dashboard', ['articles'=>$articles]);
 	}
 
 
+	public function search()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('query','Query','required');
+		if( ! $this->form_validation->run() )
+			return $this->dashboard();
 
+		$query = $this->input->post('query');
 
+		return redirect ("admin/search_results/$query");
+	}
 
-		public function search()
-		{
-			$this->load->library('form_validation');
-			$this->form_validation->set_rules('query','Query','required');
-			if( ! $this->form_validation->run() )
-				return $this->dashboard();
-
-			$query = $this->input->post('query');
-			
-			return redirect ("admin/search_results/$query");
-			// $this->load->model('articlesmodel', 'articles');
-			// $articles = $this->articles->search($query);
-
-			// $this->load->view('public/search_results',compact('articles') );
-
-		}
-
-		public function search_results( $query )
-		{
+	
+	public function search_results( $query )
+	{
 
 		$this->load->helper('form');
-	   $this->load->model('articlesmodel','articles');
-	   $this->load->library('pagination');
-			$config = [
-				'base_url'  => base_url("admin/search_results/$query"),
-				'per_page'  => 5,
-				'total_rows' => $this->articles->count_search_results( $query ),
+   		$this->load->model('articlesmodel','articles');
+   		$this->load->library('pagination');
+		$config = [
+			'base_url'  => base_url("admin/search_results/$query"),
+			'per_page'  => 5,
+			'total_rows' => $this->articles->count_search_results( $query ),
 
-				'full_tag_open' => "<ul class='pagination'>",
-				'full_tag_close' => '</ul>',
+			'full_tag_open' => "<ul class='pagination'>",
+			'full_tag_close' => '</ul>',
 
-				'first_tag_open' => '<li class="page-item">',
-				'first_tag_close' => '</li>',
+			'first_tag_open' => '<li class="page-item">',
+			'first_tag_close' => '</li>',
 
-				'uri_segment'  => 4,
-
-
-				'last_tag_open' => '<li class="page-item">',
-				'last_tag_close' => '</li>',
-
-				'next_tag_open' => '<li class="page-item">',
-				'next_tag_close' => '</li>',
-
-				'prev_tag_open' => '<li class="page-item">',
-				'prev_tag_close' => '</li>',
-
-				'num_tag_open' => '<li class="page-item">',
-				'num_tag_close' => '</li>',
-
-				'cur_tag_open' => '<li class="page-item active bg-dark text-white"><a>',
-				'cur_tag_close' => '</a></li>',
-			];
-
-			$this->pagination->initialize($config);
-
-			$articles = $this->articles->search( $query, $config['per_page'], $this->uri->segment(4) );
+			'uri_segment'  => 4,
 
 
-			$this->load->view('admin/dashboard_search_results',compact('articles') );
+			'last_tag_open' => '<li class="page-item">',
+			'last_tag_close' => '</li>',
 
-		}
+			'next_tag_open' => '<li class="page-item">',
+			'next_tag_close' => '</li>',
 
+			'prev_tag_open' => '<li class="page-item">',
+			'prev_tag_close' => '</li>',
 
+			'num_tag_open' => '<li class="page-item">',
+			'num_tag_close' => '</li>',
 
+			'cur_tag_open' => '<li class="page-item active bg-dark text-white"><a>',
+			'cur_tag_close' => '</a></li>',
+		];
 
-
-
-
-
-
-
+		$this->pagination->initialize($config);
+		$articles = $this->articles->search( $query, $config['per_page'], $this->uri->segment(4) );
+		$this->load->view('admin/dashboard_search_results',compact('articles') );
+	}
 
 	public function add_article()
 	{
@@ -137,41 +111,18 @@ class Admin extends MY_Controller{
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters("<p class='text-danger'>","</p>");
 		
-		if( $this->form_validation->run('add_article_rules') /* && $this->upload->do_upload() */ ){
+		if( $this->form_validation->run('add_article_rules') ){
 
-				$post = $this->input->post();
-				//The above line gets data entered in every field.
-				//Instead of writing it for every field as shown in the below commented line
-				// $title = $this->input->post('title');
-
-				unset($post['submit']);
-				// $this->load->model('articlesmodel','articles');
-				// The above line is commented because it is now written in common in constructor.
-
-
-
-				// $data = $this->upload->data();
-				// $file_path = base_url("uploads/". $data['raw_name'] . $data['file_ext']);
-				//The above line displays complete path and below line display just the file name
-				// $file_path = ($data['file_name']);
-				// $post['file_path']= $file_path ; 
-
-
-
-				return $this->_flashAndRedirect(
-						$this->articles->add_article($post),
-						 "Task added Successfully",
-						 "Task failed to add. Please try again."
-				);
-
-
-
+			$post = $this->input->post();
+			unset($post['submit']);
+			return $this->_flashAndRedirect(
+					$this->articles->add_article($post),
+					 "Task added Successfully",
+					 "Task failed to add. Please try again."
+			);
 		}
 		else{
-			// $upload_error = $this->upload->display_errors();
-
 			$this->load->view('admin/add_article', /* compact('upload_error') */);
-
 		}
 	}
 
@@ -179,8 +130,6 @@ class Admin extends MY_Controller{
 
 	public function edit_article($article_id)
 	   {
-	   	// $this->load->model('articlesmodel','articles');
-	   	// The above line is commented because it is now written in common in constructor.
 	   	$article = $this->articles->find_article($article_id);
 	   	$this->load->view('admin/edit_article', ['article'=>$article]);
 	   }
@@ -200,42 +149,15 @@ class Admin extends MY_Controller{
 		if( $this->form_validation->run('add_article_rules') /* && $this->upload->do_upload() */ ){
 
 				$post = $this->input->post();
-				//The above line gets data entered in every field.
-				//Instead of writing it for every field as shown in the below commented line
-				// $title = $this->input->post('title');
-
-
 				unset($post['submit']);
-				// $this->load->model('articlesmodel','articles');
-				// The above line is commented because it is now written in common in constructor.
-
-
-
-
-				// $data = $this->upload->data();
-				// $file_path = base_url("uploads/". $data['raw_name'] . $data['file_ext']);
-				// $file_path = ($data['file_name']);	
-				// $post['file_path']= $file_path ; 
-
-
-
-
-
 				return $this->_flashAndRedirect(
 						$this->articles->update_article($article_id, $post),
 						"Task Updated Successfully.",
 						"Task failed to update. Please try again."
 				);
-				
 
-
-		}else{
-			
-			
-			// $upload_error = $this->upload->display_errors();
-			$this->load->view('admin/edit_article'/*, compact('upload_error') */);
-			
-			
+		} else {
+			$this->load->view('admin/edit_article');	
 		}
 	}
 
@@ -248,8 +170,6 @@ class Admin extends MY_Controller{
 	{
 		
 		$article_id = $this->input->post('article_id');
-		// $this->load->model('articlesmodel','articles');
-		// The above line is commented because it is now written in common in constructor.
 
 		return $this->_flashAndRedirect(
 			$this->articles->delete_article($article_id),
@@ -280,8 +200,6 @@ class Admin extends MY_Controller{
 			return redirect('login');
 
 		$this->load->model('articlesmodel','articles');
-		// The above line is written here (in constructor), because it was repeating in every function above, hence to avoid repeating code as per DRY concept. 
-
 	}
 
 
